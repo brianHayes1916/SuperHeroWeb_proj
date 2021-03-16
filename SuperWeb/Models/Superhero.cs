bc33e03c6d84cb18c70173fd480863a092ae387b
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-16 13:45:30 -0500

    Superhero object created without connection to database.

---
+++ SuperWeb/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperWeb.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchPhrase { get; set; }
+
+    }
+}
