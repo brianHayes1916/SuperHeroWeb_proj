702c0df714096331a9ae2f0c2c75ce34a7a2833d
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-16 13:59:06 -0500

    Added the superhero model to the database

--- SuperWeb/Data/ApplicationDbContext.cs
+++ SuperWeb/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace SuperWeb.Data
             : base(options)
         {
         }
+
+        public DbSet<Superhero> Heros { get; set; }
     }
 }
---
+++ SuperWeb/Data/Migrations/20210316185733_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperWeb.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    alterEgo = table.Column<string>(nullable: true),
+                    primaryAbility = table.Column<string>(nullable: true),
+                    secondaryAbility = table.Column<string>(nullable: true),
+                    catchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heros", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Heros");
+        }
+    }
+}
